357059c108ce89ab946f0687a36c93db034c238f
MestreLion <linux@rodrigosilva.com>
2014-08-13 22:21:08 -0300

    add some minimal instrumentation

--- barebones.py
+++ barebones.py
@@ -2,20 +2,24 @@
 
 import pygame
 
+FPS = 60
+
 position = [100, 200]
 velocity = [7, 17]
 radius = 50
 color = (0, 255, 0)  # green
-
+bgcolor = (255, 255, 255) # white
 
 pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((800, 600))
 background = pygame.Surface(screen.get_size())
+background.fill(bgcolor)
 screen.blit(background, (0,0))
 
 ball = pygame.Surface(2*[radius*2])
+ball.set_colorkey((0,0,0))
 pygame.draw.circle(ball, color, 2*(radius,), radius)
 bounds = (screen.get_size()[0] - 2 * radius,
           screen.get_size()[1] - 2 * radius)
@@ -24,6 +28,8 @@ rect.x = position[0]
 rect.y = position[1]
 
 done = False
+frames = 0
+fpslist = []
 while not done:
     for event in pygame.event.get():
         if event.type in [pygame.QUIT, pygame.KEYDOWN]:
@@ -39,6 +45,15 @@ while not done:
     rect.y = position[1]
     screen.blit(ball, rect)
     pygame.display.update()
-    clock.tick(60)
+
+    frames += 1
+    if frames == (FPS or 60):
+        fps = clock.get_fps()
+        fpslist.append(fps)
+        pygame.display.set_caption("FPS: %d" % fps)
+        frames = 0
+    clock.tick(FPS)
 
 pygame.quit()
+if fpslist:
+    print min(fpslist), sum(fpslist) / len(fpslist), max(fpslist)
